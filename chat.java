73c2cd950991e352ab9d5ee20ae03cb880412137
Xuanson On <hello@xuansonon.com>
2016-11-12 09:57:36 +1100

    add the ability to ask users for details

--- chat.java
+++ chat.java
@@ -16,24 +16,36 @@ public class LANChat {
   int chatPort;
 
   /* Empty Initialiser so we can set the chat variables as we go. */
-  public LANChat() {};
+  public LANChat(String aUsername, InetAddress aChatKey, int aChatPort) {
+    userName = aUsername;
+    chatKey = aChatKey;
+    chatPort = aChatPort;
+  };
 
   public static void askForUserDetails(LANChat aClient) {
     BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
-    //Get the input
-    String IUsername = new String(inputReader.readLine());
-    String IChatKey = new String(inputReader.readLine());
-    String IChatPort = new String(inputReader.readLine());
-    //Conver the required inputs - Type Safing
-    InetAddress CChatKey = InetAddress.getByName(IUsername);
-    int CChatPort = Integer.parseInt(IChatPort);
-    aClient.userName = IUsername;
-    aClient.chatKey = CChatKey;
-    aClient.chatPort = CChatPort;
+    String IUsername = new String();
+    InetAddress IChatKey;
+    int IChatPort = new Integer();
+
+    try {
+      IUsername = new String(inputReader.readLine());
+      IChatKey = InetAddress.getByName(new String(inputReader.readLine()));
+      IChatPort = Integer.parseInt(new String(inputReader.readLine()));
+    } catch(NumberFormatterException e) {
+      System.out.println("Error: Port Number is Invalid. Required: Port Number (Integer). Exitting...")
+      System.exit(0);
+    } catch(UnknownHostException e) {
+      System.out.println("Error: Invalid IP Address. Required: IP Address.");
+      System.exit(0);
+    }
+
+    aClient = new LANChat(IUsername, IChatKey, IChatPort);
+
   }
 
   public static void main(String[] args) {
-    LANChat userChatClient = new LANChat();
+    LANChat userChatClient;
     askForUserDetails(userChatClient);
   }
 }
