efd4772ba8975509ab977094ff26a08f17b06e2b
Xuanson On <hello@xuansonon.com>
2016-11-12 11:20:00 +1100

    add functionality to add user details to chat

--- chat.java
+++ chat.java
@@ -6,46 +6,45 @@ import java.net.*;
   public chat and enables private whispers between users.
   Uses TCP for private whipsers and a UDP Multicast for
   public chats.
-  Version: 0.9 (11 November 2016)
+  Version: 0.901
 */
 
-public class LANChat {
+class LANChat {
   //Chat variables
   String userName;
   InetAddress chatKey;
   int chatPort;
 
   /* Empty Initialiser so we can set the chat variables as we go. */
-  public LANChat(String aUsername, InetAddress aChatKey, int aChatPort) {
-    userName = aUsername;
-    chatKey = aChatKey;
-    chatPort = aChatPort;
-  };
+  public LANChat() {};
 
   public static void askForUserDetails(LANChat aClient) {
     BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
     String IUsername = new String();
     InetAddress IChatKey;
-    int IChatPort = new Integer();
+    int IChatPort = 0;
 
     try {
       IUsername = new String(inputReader.readLine());
       IChatKey = InetAddress.getByName(new String(inputReader.readLine()));
       IChatPort = Integer.parseInt(new String(inputReader.readLine()));
-    } catch(NumberFormatterException e) {
-      System.out.println("Error: Port Number is Invalid. Required: Port Number (Integer). Exitting...")
+      aClient.userName = IUsername;
+      aClient.chatKey = IChatKey;
+      aClient.chatPort = IChatPort;
+    } catch(NumberFormatException e) {
+      System.out.println("Error: Port Number is Invalid. Required: Port Number (Integer). Exitting...");
       System.exit(0);
     } catch(UnknownHostException e) {
-      System.out.println("Error: Invalid IP Address. Required: IP Address.");
+      System.out.println("Error: Invalid IP Address. Required: IP Address. Exitting...");
+      System.exit(0);
+    } catch(IOException e) {
+      System.out.println("Could not handle the IO. Exitting...");
       System.exit(0);
     }
-
-    aClient = new LANChat(IUsername, IChatKey, IChatPort);
-
   }
 
   public static void main(String[] args) {
-    LANChat userChatClient;
+    LANChat userChatClient = new LANChat();
     askForUserDetails(userChatClient);
   }
 }
